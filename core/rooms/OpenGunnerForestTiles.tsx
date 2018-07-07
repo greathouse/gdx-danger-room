39aa55682d7a45e3e08864553f94e4e0d94dea06
Robert G. Greathouse <greathouse.robert@gmail.com>
2018-07-06 21:47:24 -0500

    Stuff

--- core/rooms/OpenGunnerForestTiles.tsx
+++ core/rooms/OpenGunnerForestTiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="OpenGunnerForestTiles" tilewidth="50" tileheight="50" tilecount="897" columns="39">
- <image source="../assets/gunner/OpenGunnerForestTiles.png" width="1972" height="1184"/>
+<tileset name="Gunner" tilewidth="50" tileheight="50" tilecount="90" columns="6">
+ <image source="../assets/gunner/tileOutputs/tileset-final.png" width="300" height="750"/>
 </tileset>
---
+++ core/rooms/tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset1" tilewidth="50" tileheight="50" tilecount="42" columns="3">
+ <image source="../assets/gunner/tileOutputs/tileset1.png" width="150" height="700"/>
+</tileset>
