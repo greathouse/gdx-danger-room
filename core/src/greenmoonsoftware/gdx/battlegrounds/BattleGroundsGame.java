02d4c3752c70c1f2cc944c3ac9feb05489337db6
Robert Greathouse <robert@roberts-mbp.lan>
2018-12-22 14:33:55 -0600

    Extract player class

--- core/src/greenmoonsoftware/gdx/battlegrounds/BattleGroundsGame.java
+++ core/src/greenmoonsoftware/gdx/battlegrounds/BattleGroundsGame.java
@@ -1,27 +1,21 @@
 package greenmoonsoftware.gdx.battlegrounds;
 
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
 import greenmoonsoftware.gdx.GreenMoonGame;
 import greenmoonsoftware.gdx.ailounge.PlainScreen;
 
 public class BattleGroundsGame extends GreenMoonGame {
 
-    private Texture pixmaptex;
+    private Player player1;
 
     public void onCreate() {
         this.setScreen(new PlainScreen(this));
-
-        Pixmap pixmap = new Pixmap( 16, 16, Pixmap.Format.RGBA8888 );
-        pixmap.setColor( 0, 1, 0, 0.75f );
-        pixmap.fillCircle( 8, 8, 8 );
-        pixmaptex = new Texture( pixmap );
-        pixmap.dispose();
+        player1 = new Player(this, 100, 100);
+        player2 = new Player(this, 200, 100);
     }
 
     @Override
     public void render() {
         super.render();
-        doInBatch(batch -> batch.draw(pixmaptex, 100, 100));
+        player1.render();
     }
 }
